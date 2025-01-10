370803d043de824cccf76461574c340904aa57a5
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-01-09 21:32:19 -0800

    axios

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+
+
+app.use(cors())
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
